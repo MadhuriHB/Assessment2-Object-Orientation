e9889d635d16f3c96b65b947aec15c57dbf8f5f1
MadhuriHB <madhurisubhash2002@gmail.com>
2016-07-23 22:43:54 -0700

    Added answers to part1

--- assessment.py
+++ assessment.py
@@ -3,17 +3,61 @@ Part 1: Discussion
 
 1. What are the three main design advantages that object orientation
    can provide? Explain each concept.
+   
+   1. Encapsulation:
 
-2. What is a class?
+        It means hiding data.
+        In an object oriented python program, you can restrict access to
+        methods and variables through Encapsulation.
+        This can prevent the data from being modified
+        by accident.
+        Abstracton is achieved through Encapsulation.
+
+
+    2. Inheritance:
+
+        The transfer of the characteristics of a class to 
+        other classes that are derived from it.
+
+
+    3. Polymorphism:
+
+        In a child class we can change how some methods work whilst keeping
+        the same name. We call this polymorphism and it is useful because
+        we do not want to keep introducing new method names for 
+        functionality that is pretty similar in each class. 
+
+2. What is a class
+   Class can be thought of as blueprint for creating objects.
+
+
 
 3. What is an instance attribute?
+   Instance attributes are owned by the specific instances of a class.
+   This means for two different instances the instance attributes are 
+   usually different. 
+
 
 4. What is a method?
+   Methods are functions defined within the class.
+
+        A method differs from a function only in two aspects:
+
+            1. It belongs to a class and it is defined within a class.
+            The first parameter in the definition of a method has to be a 
+            reference "self" to the instance of the class.
+        
+            2. A method is called without this parameter "self".
 
 5. What is an instance in object orientation?
+    Instance is an object created from the class.
 
 6. How is a class attribute different than an instance attribute?
    Give an example of when you might use each.
+   
+        A class attribute is a piece of data on the class itself. When you ask 
+        for it on an instance, it finds it on the class.
+        An instance attribute is set directly on the object.  
 
 
 """
@@ -24,14 +68,12 @@ class Student(object):
          last name and address """    
   
     def __init__(self, firstname, lastname, address):
-        
         self.first_name = firstname
         self.last_name = lastname
         self.address = address
     
 
 class Question(object):
-    # Questions = []
     
     def __init__(self, question, answer):
         self.question = question
@@ -77,10 +119,8 @@ class Quiz(Exam):
 
         super(Quiz, self).administer()
         if self.score <= self.number_of_questions/2:
-            
             return "False"
         else:
-
             return "True"
 
 
